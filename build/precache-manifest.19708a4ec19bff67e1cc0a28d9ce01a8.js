1fa5edc75abecfa3d878d8bb1eb95edb41f6522b
tatar88t <tatar_88@bk.ru>
2020-08-25 12:47:10 +0300

    commit

---
+++ build/precache-manifest.19708a4ec19bff67e1cc0a28d9ce01a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1540f7a308732126744bc622b5494233",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "7f72e18b4d3dd14d0480",
+    "url": "/cats/static/css/main.75a6d258.chunk.css"
+  },
+  {
+    "revision": "0b56f72632ad8e2c5db4",
+    "url": "/cats/static/js/2.1f3becd2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cats/static/js/2.1f3becd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f72e18b4d3dd14d0480",
+    "url": "/cats/static/js/main.fe85aa41.chunk.js"
+  },
+  {
+    "revision": "4990938f7f8b05e8172c",
+    "url": "/cats/static/js/runtime-main.cfe87a95.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/cats/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "fca2adbb34d8a2b0bac7516ec5d40e7f",
+    "url": "/cats/static/media/bg.fca2adbb.jpg"
+  },
+  {
+    "revision": "4fab8f3e475fc60f03e475aabd22263f",
+    "url": "/cats/static/media/cat.4fab8f3e.png"
+  }
+]);
